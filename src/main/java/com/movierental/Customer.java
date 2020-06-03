521d3083efb1a047f131db2443a3989115d9e5ee
abhilashak2k <abhilashak2k@gmail.com>
2020-06-03 14:08:23 +0530

    removed feature envy

--- src/main/java/com/movierental/Customer.java
+++ src/main/java/com/movierental/Customer.java
@@ -31,7 +31,7 @@ public class Customer {
       double thisAmount = 0;
       //determine amounts for each line
       
-      thisAmount = amountFor(each, thisAmount);
+      thisAmount = each.amount(thisAmount);
       
       // add frequent renter points
       frequentRenterPoints++;
@@ -52,26 +52,5 @@ public class Customer {
         + " frequent renter points";
     return result;
   }
-
-private double amountFor(Rental each, double thisAmount) {
-	
-	switch (each.getMovie().getPriceCode()) {
-        case Movie.REGULAR:
-          thisAmount += 2;
-          if (each.getDaysRented() > 2)
-            thisAmount += (each.getDaysRented() - 2) * 1.5;
-          break;
-        case Movie.NEW_RELEASE:
-          thisAmount += each.getDaysRented() * 3;
-          break;
-        case Movie.CHILDRENS:
-          thisAmount += 1.5;
-          if (each.getDaysRented() > 3)
-            thisAmount += (each.getDaysRented() - 3) * 1.5;
-          break;
-      }
-	return thisAmount;
-	
-	}
 }
 
